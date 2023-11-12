1135665a8dfdfaa14f1635ffdaa296c0cc3f1618
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-11-12 17:28:01 +0100

    feat: multilanguage support

--- src/app.js
+++ src/app.js
@@ -1,16 +1,58 @@
-function greet(name) {
-    if(isUndefinedOrNull(name) || isEmptyString(name)) return "Hello, my friend.";
-    
-    if(isAnArray(name)){
-        if(name.length === 2) return `Hello, ${name[0]} and ${name[1]}.`;
-        return arrayGreeting(name);
-    }
-    if(isUpperCase(name)) return `HELLO, ${name}!`;
-
-    return "Hello, " + name + ".";
+const languages = {
+    "en" : {
+        isNull: "my friend.",
+        helloLower: "Hello",
+        helloUpper: "HELLO",
+        andLower: "and",
+        andUpper: "AND"
+    },
+    "nl" : {
+        isNull: "mijn vriend.",
+        helloLower: "Hallo",
+        helloUpper: "HALLO",
+        andLower: "en",
+        andUpper: "EN"
+    },
+    "fr" : {
+        isNull: "mon ami.",
+        helloLower: "Bonjour",
+        helloUpper: "BONJOUR",
+        andLower: "et",
+        andUpper: "ET"
+    },
 }
 
 
+function greet(name) {
+    const { language, index } = findLanguage(name);
+    if(index !== -1) name.splice(index, 1);
+
+    if(!isUndefinedOrNull(name) && isAnArray(name) && name.length === 1) name = name[0];
+    
+    if(isUndefinedOrNull(name) || isEmptyString(name)) return `${language.helloLower}, ${language.isNull}`;
+    
+    if(isAnArray(name) && name.length > 1){
+        return arrayGreeting(name, language);
+    }
+    if(isUpperCase(name)) return `${language.helloUpper}, ${name}!`;
+
+    return `${language.helloLower}, ${name}.`;
+}
+
+function isLanguageSupported(language) {
+    return languages[language] !== undefined;
+}
+
+function findLanguage(name) {
+    if (isUndefinedOrNull(name)) return { language: languages["en"], index: -1 };
+
+    for (let i = 0; i < name.length; i++) {
+        if (isLanguageSupported(name[i])) {
+            return { language: languages[name[i]], index: i };
+        }
+    }
+    return { language: languages["en"], index: -1 };
+}
 
 function isUndefinedOrNull(name) {
     return name === undefined || name === null;
@@ -27,23 +69,21 @@ function isAnArray(name) {
     return typeof name !== "string";
 }
 
-function arrayGreeting(name) {
+function arrayGreeting(name, language) {
     let upperCase;
-    let message = "Hello";
+    let message = language.helloLower;
     for(i = 0; i < name.length ; i += 1) {
+        if(isLanguageSupported(name[i])) continue;
+
         if(isUpperCase(name[i])) {
             upperCase = name[i];
             continue;
         }
-
-
-        if(i === name.length-1) message += ` and ${name[i]}.`;
+        if(i === name.length-1) message += ` ${language.andLower} ${name[i]}.`;
         else message += `, ${name[i]}`;
     }
-    if(upperCase!==undefined) message+= ` AND HELLO ${upperCase}!`;
+    if(upperCase!==undefined) message+= ` ${language.andUpper} ${language.helloUpper} ${upperCase}!`;
     return message;
 }
-
-
 
 module.exports = greet;
--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,8 +1,16 @@
 
 const greet = require("../app");
 
+
+// English tests (by default)
 test("Should return Hello, Bob.", () => {
     const result = greet("Bob");
+    expect(result).toBe("Hello, Bob.");
+});
+
+
+test("Should return Hello, Bob.", () => {
+    const result = greet(["Bob", "en"]);
     expect(result).toBe("Hello, Bob.");
 });
 
@@ -63,4 +71,76 @@ test("Should return Hello, Amy and Charlotte. AND HELLO BRIAN!", () => {
 test("Should return Hello, Thanathos an Hypnos. AND HELLO KRATOS!", () => {
     const result = greet(["KRATOS", "Thanathos", "Hypnos"]);
     expect(result).toBe("Hello, Thanathos and Hypnos. AND HELLO KRATOS!");
+});
+
+
+
+// French tests
+
+test("Should return Bonjour, Bob.", () => {
+    const result = greet(["Bob", "fr"]);
+    expect(result).toBe("Bonjour, Bob.");
+});
+
+
+test("Should return Bonjour, mon ami", () => {
+    const result = greet([null, "fr"]);
+    expect(result).toBe("Bonjour, mon ami.");
+});
+
+
+test("Should return BONJOUR, JERRY!", () => {
+    const result = greet(["JERRY", "fr"]);
+    expect(result).toBe("BONJOUR, JERRY!");
+});
+
+test("Should return Bonjour, Jill et Jane.", () => {
+    const result = greet(["Jill", "Jane", "fr"]);
+    expect(result).toBe("Bonjour, Jill et Jane.");
+});
+
+
+test("Should return Bonjour, Amy, Brian et Charlotte.", () => {
+    const result = greet(["Amy", "Brian","Charlotte", "fr"]);
+    expect(result).toBe("Bonjour, Amy, Brian et Charlotte.");
+});
+
+test("Should return Bonjour, Amy et Charlotte. ET BONJOUR BRIAN!", () => {
+    const result = greet(["Amy", "BRIAN", "Charlotte", "fr"]);
+    expect(result).toBe("Bonjour, Amy et Charlotte. ET BONJOUR BRIAN!");
+});
+
+
+// DUTCH TESTS
+test("Should return Hallo, Bob.", () => {
+    const result = greet(["Bob", "nl"]);
+    expect(result).toBe("Hallo, Bob.");
+});
+
+
+test("Should return Hallo, mon ami", () => {
+    const result = greet([null, "nl"]);
+    expect(result).toBe("Hallo, mijn vriend.");
+});
+
+
+test("Should return HALLO, JERRY!", () => {
+    const result = greet(["JERRY", "nl"]);
+    expect(result).toBe("HALLO, JERRY!");
+});
+
+test("Should return Hallo, Jill en Jane.", () => {
+    const result = greet(["Jill", "Jane", "nl"]);
+    expect(result).toBe("Hallo, Jill en Jane.");
+});
+
+
+test("Should return Hallo, Amy, Brian en Charlotte.", () => {
+    const result = greet(["Amy", "Brian","Charlotte", "nl"]);
+    expect(result).toBe("Hallo, Amy, Brian en Charlotte.");
+});
+
+test("Should return Hallo, Amy en Charlotte. EN HALLO BRIAN!", () => {
+    const result = greet(["Amy", "BRIAN", "Charlotte", "nl"]);
+    expect(result).toBe("Hallo, Amy en Charlotte. EN HALLO BRIAN!");
 });
