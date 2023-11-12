b4fcb36e07ff9d64999091f81ce4c1e282adf344
axel-lefranc-vinci <axel.lefranc@student.vinci.be>
2023-11-12 15:01:24 +0100

    branch was created but was not added in the remote repo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 function greet(name) {
     if(isUndefinedOrNull(name) || isEmptyString(name)) return "Hello, my friend.";
-    
     if( typeof name !== "string"){
         return `Hello, ${name[0]} and ${name[1]}.`
     }
     if(isUpperCase(name)) return "HELLO, "+name+"!";
-
     return "Hello, " + name + ".";
 }
 
