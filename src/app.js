1bb3915b91fd858a9bf7f1d90bab91c5c95149d5
Steedlan <40958752+Steedlan@users.noreply.github.com>
2023-11-16 18:08:44 +0100

    fix: eslint

--- src/app.js
+++ src/app.js
@@ -20,73 +20,73 @@ const languages = {
         andLower: 'et',
         andUpper: 'ET',
     },
-}
+};
 
 function greet(name) {
-    const { language, index } = findLanguage(name)
-    if (index !== -1) name.splice(index, 1)
+    const { language, index } = findLanguage(name);
+    if (index !== -1) name.splice(index, 1);
 
     if (!isUndefinedOrNull(name) && isAnArray(name) && name.length === 1)
-        name = name[0]
+        name = name[0];
 
     if (isUndefinedOrNull(name) || isEmptyString(name))
-        return `${language.helloLower}, ${language.isNull}`
+        return `${language.helloLower}, ${language.isNull}`;
 
     if (isAnArray(name) && name.length > 1) {
-        return arrayGreeting(name, language)
+        return arrayGreeting(name, language);
     }
-    if (isUpperCase(name)) return `${language.helloUpper}, ${name}!`
+    if (isUpperCase(name)) return `${language.helloUpper}, ${name}!`;
 
-    return `${language.helloLower}, ${name}.`
+    return `${language.helloLower}, ${name}.`;
 }
 
 function isLanguageSupported(language) {
-    return languages[language] !== undefined
+    return languages[language] !== undefined;
 }
 
 function findLanguage(name) {
-    if (isUndefinedOrNull(name)) return { language: languages['en'], index: -1 }
+    if (isUndefinedOrNull(name)) return { language: languages['en'], index: -1 };
 
     for (let i = 0; i < name.length; i++) {
         if (isLanguageSupported(name[i])) {
-            return { language: languages[name[i]], index: i }
+            return { language: languages[name[i]], index: i };
         }
     }
-    return { language: languages['en'], index: -1 }
+    return { language: languages['en'], index: -1 };
 }
 
 function isUndefinedOrNull(name) {
-    return name === undefined || name === null
+    return name === undefined || name === null;
 }
 
 function isEmptyString(name) {
-    return name === ''
+    return name === '';
 }
 function isUpperCase(name) {
-    return name === name.toUpperCase()
+    return name === name.toUpperCase();
 }
 
 function isAnArray(name) {
-    return typeof name !== 'string'
+    return typeof name !== 'string';
 }
 
 function arrayGreeting(name, language) {
-    let upperCase
-    let message = language.helloLower
+    let upperCase;
+    let message = language.helloLower;
     for (i = 0; i < name.length; i += 1) {
-        if (isLanguageSupported(name[i])) continue
+        if (isLanguageSupported(name[i])) continue;
 
         if (isUpperCase(name[i])) {
-            upperCase = name[i]
-            continue
+            upperCase = name[i];
+            continue;
         }
         if (i === name.length - 1)
-            message += ` ${language.andLower} ${name[i]}.`
-        else message += `, ${name[i]}`
+            message += ` ${language.andLower} ${name[i]}.`;
+        else message += `, ${name[i]}`;
     }
     if (upperCase !== undefined)
-        message += ` ${language.andUpper} ${language.helloUpper} ${upperCase}!`
-    return message
+        message += ` ${language.andUpper} ${language.helloUpper} ${upperCase}!`;
+    return message;
 }
 
-module.exports = greet
+module.exports = greet;
--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,134 +1,134 @@
-const greet = require('../app')
+const greet = require('../app');
 
 // English tests (by default)
 test('Should return Hello, Bob.', () => {
-    const result = greet('Bob')
-    expect(result).toBe('Hello, Bob.')
-})
+    const result = greet('Bob');
+    expect(result).toBe('Hello, Bob.');
+});
 
 test('Should return Hello, Bob.', () => {
-    const result = greet(['Bob', 'en'])
-    expect(result).toBe('Hello, Bob.')
-})
+    const result = greet(['Bob', 'en']);
+    expect(result).toBe('Hello, Bob.');
+});
 
 test('Should return Hello, Jean-Kevin.', () => {
-    const result = greet('Jean-Kevin')
-    expect(result).toBe('Hello, Jean-Kevin.')
-})
+    const result = greet('Jean-Kevin');
+    expect(result).toBe('Hello, Jean-Kevin.');
+});
 
 test('Should return Hello, my friend', () => {
-    const result = greet(null)
-    expect(result).toBe('Hello, my friend.')
-})
+    const result = greet(null);
+    expect(result).toBe('Hello, my friend.');
+});
 
 test('Should return Hello, my friend', () => {
-    const result = greet()
-    expect(result).toBe('Hello, my friend.')
-})
+    const result = greet();
+    expect(result).toBe('Hello, my friend.');
+});
 
 test('Should return Hello, my friend', () => {
-    const result = greet('')
-    expect(result).toBe('Hello, my friend.')
-})
+    const result = greet('');
+    expect(result).toBe('Hello, my friend.');
+});
 
 test('Should return HELLO, JERRY!', () => {
-    const result = greet('JERRY')
-    expect(result).toBe('HELLO, JERRY!')
-})
+    const result = greet('JERRY');
+    expect(result).toBe('HELLO, JERRY!');
+});
 
 test('Should return HELLO, KENOBI!', () => {
-    const result = greet('KENOBI')
-    expect(result).toBe('HELLO, KENOBI!')
-})
+    const result = greet('KENOBI');
+    expect(result).toBe('HELLO, KENOBI!');
+});
 
 test('Should return Hello, Jill and Jane.', () => {
-    const result = greet(['Jill', 'Jane'])
-    expect(result).toBe('Hello, Jill and Jane.')
-})
+    const result = greet(['Jill', 'Jane']);
+    expect(result).toBe('Hello, Jill and Jane.');
+});
 
 test('Should return Hello, Kratos and Thanathos.', () => {
-    const result = greet(['Kratos', 'Thanathos'])
-    expect(result).toBe('Hello, Kratos and Thanathos.')
-})
+    const result = greet(['Kratos', 'Thanathos']);
+    expect(result).toBe('Hello, Kratos and Thanathos.');
+});
 
 test('Should return Hello, Amy, Brian and Charlotte.', () => {
-    const result = greet(['Amy', 'Brian', 'Charlotte'])
-    expect(result).toBe('Hello, Amy, Brian and Charlotte.')
-})
+    const result = greet(['Amy', 'Brian', 'Charlotte']);
+    expect(result).toBe('Hello, Amy, Brian and Charlotte.');
+});
 
 test('Should return Hello, Kratos, Thanathos and Hypnos.', () => {
-    const result = greet(['Kratos', 'Thanathos', 'Hypnos'])
-    expect(result).toBe('Hello, Kratos, Thanathos and Hypnos.')
-})
+    const result = greet(['Kratos', 'Thanathos', 'Hypnos']);
+    expect(result).toBe('Hello, Kratos, Thanathos and Hypnos.');
+});
 
 test('Should return Hello, Amy and Charlotte. AND HELLO BRIAN!', () => {
-    const result = greet(['Amy', 'BRIAN', 'Charlotte'])
-    expect(result).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!')
-})
+    const result = greet(['Amy', 'BRIAN', 'Charlotte']);
+    expect(result).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!');
+});
 test('Should return Hello, Thanathos an Hypnos. AND HELLO KRATOS!', () => {
-    const result = greet(['KRATOS', 'Thanathos', 'Hypnos'])
-    expect(result).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS!')
-})
+    const result = greet(['KRATOS', 'Thanathos', 'Hypnos']);
+    expect(result).toBe('Hello, Thanathos and Hypnos. AND HELLO KRATOS!');
+});
 
 // French tests
 
 test('Should return Bonjour, Bob.', () => {
-    const result = greet(['Bob', 'fr'])
-    expect(result).toBe('Bonjour, Bob.')
-})
+    const result = greet(['Bob', 'fr']);
+    expect(result).toBe('Bonjour, Bob.');
+});
 
 test('Should return Bonjour, mon ami', () => {
-    const result = greet([null, 'fr'])
-    expect(result).toBe('Bonjour, mon ami.')
-})
+    const result = greet([null, 'fr']);
+    expect(result).toBe('Bonjour, mon ami.');
+});
 
 test('Should return BONJOUR, JERRY!', () => {
-    const result = greet(['JERRY', 'fr'])
-    expect(result).toBe('BONJOUR, JERRY!')
-})
+    const result = greet(['JERRY', 'fr']);
+    expect(result).toBe('BONJOUR, JERRY!');
+});
 
 test('Should return Bonjour, Jill et Jane.', () => {
-    const result = greet(['Jill', 'Jane', 'fr'])
-    expect(result).toBe('Bonjour, Jill et Jane.')
-})
+    const result = greet(['Jill', 'Jane', 'fr']);
+    expect(result).toBe('Bonjour, Jill et Jane.');
+});
 
 test('Should return Bonjour, Amy, Brian et Charlotte.', () => {
-    const result = greet(['Amy', 'Brian', 'Charlotte', 'fr'])
-    expect(result).toBe('Bonjour, Amy, Brian et Charlotte.')
-})
+    const result = greet(['Amy', 'Brian', 'Charlotte', 'fr']);
+    expect(result).toBe('Bonjour, Amy, Brian et Charlotte.');
+});
 
 test('Should return Bonjour, Amy et Charlotte. ET BONJOUR BRIAN!', () => {
-    const result = greet(['Amy', 'BRIAN', 'Charlotte', 'fr'])
-    expect(result).toBe('Bonjour, Amy et Charlotte. ET BONJOUR BRIAN!')
-})
+    const result = greet(['Amy', 'BRIAN', 'Charlotte', 'fr']);
+    expect(result).toBe('Bonjour, Amy et Charlotte. ET BONJOUR BRIAN!');
+});
 
 // DUTCH TESTS
 test('Should return Hallo, Bob.', () => {
-    const result = greet(['Bob', 'nl'])
-    expect(result).toBe('Hallo, Bob.')
-})
+    const result = greet(['Bob', 'nl']);
+    expect(result).toBe('Hallo, Bob.');
+});
 
 test('Should return Hallo, mon ami', () => {
-    const result = greet([null, 'nl'])
-    expect(result).toBe('Hallo, mijn vriend.')
-})
+    const result = greet([null, 'nl']);
+    expect(result).toBe('Hallo, mijn vriend.');
+});
 
 test('Should return HALLO, JERRY!', () => {
-    const result = greet(['JERRY', 'nl'])
-    expect(result).toBe('HALLO, JERRY!')
-})
+    const result = greet(['JERRY', 'nl']);
+    expect(result).toBe('HALLO, JERRY!');
+});
 
 test('Should return Hallo, Jill en Jane.', () => {
-    const result = greet(['Jill', 'Jane', 'nl'])
-    expect(result).toBe('Hallo, Jill en Jane.')
-})
+    const result = greet(['Jill', 'Jane', 'nl']);
+    expect(result).toBe('Hallo, Jill en Jane.');
+});
 
 test('Should return Hallo, Amy, Brian en Charlotte.', () => {
-    const result = greet(['Amy', 'Brian', 'Charlotte', 'nl'])
-    expect(result).toBe('Hallo, Amy, Brian en Charlotte.')
-})
+    const result = greet(['Amy', 'Brian', 'Charlotte', 'nl']);
+    expect(result).toBe('Hallo, Amy, Brian en Charlotte.');
+});
 
 test('Should return Hallo, Amy en Charlotte. EN HALLO BRIAN!', () => {
-    const result = greet(['Amy', 'BRIAN', 'Charlotte', 'nl'])
-    expect(result).toBe('Hallo, Amy en Charlotte. EN HALLO BRIAN!')
-})
+    const result = greet(['Amy', 'BRIAN', 'Charlotte', 'nl']);
+    expect(result).toBe('Hallo, Amy en Charlotte. EN HALLO BRIAN!');
+});
